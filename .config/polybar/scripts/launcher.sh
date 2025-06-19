583a668ad1e3c664ad8d30e81bf5321140229aba
Klejdi <klejdijanora@gmail.com>
2025-06-19 17:09:46 +0200

    Uploaded launcher.sh , powermenu.sh

---
+++ .config/polybar/scripts/launcher.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# powermenu.sh
+# author: Klejdi Janari (klejdiLOL)
+# OpenMandriva
+# https://forum.openmandriva.org/
+# Location: ~/.config/polybar/scripts/powermenu.sh
+
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/scripts/rofi/launcher.rasi
