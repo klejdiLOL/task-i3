8b756f0f7960c469495dd95a53fd7e561f828216
Klejdi <klejdijanora@gmail.com>
2025-06-21 09:29:49 +0200

    Delete .config/rofi/launcher.sh

--- .config/rofi/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# launcher.sh
-# author: Klejdi Janari (klejdiLOL)
-# OpenMandriva
-# https://forum.openmandriva.org/
-# Location: ~/.config/polybar/rofi/launcher.sh
-
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/rofi/rofi/launcher.rasi
